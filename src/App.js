b5bdd28e67c8327eef77ef48e93a6c2af1df16ab
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2024-01-20 15:16:44 +0900

    opt: Preload first image of modal to prevent incorrect layout during initial loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
@@ -13,6 +13,12 @@ function App() {
     const onModalToggleButtonMouseEnter = () => {
         const component = import('./components/ImageModal');
     }
+
+    useEffect(() => {
+        // Preload first image of modal to prevent incorrect layout on initial open
+        const image = new Image();
+        image.src = 'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800'
+    }, [])
 
     return (
         <div className="App">
